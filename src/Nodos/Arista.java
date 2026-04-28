0e68bfae333aa0e381d5aab07f2404b8ee897bee
Raul Manteca <raul.manteca@edu.uah.es>
2026-04-29 00:13:40 +0200

    NodoGrafo y Arista para responder a la primera pregunta de grafos, falta la clase padre y del GrafoConocimientos

---
+++ src/Nodos/Arista.java
@@ -0,0 +1,33 @@
+package Nodos;
+
+public class Arista implements Comparable<Arista> {
+
+    private NodoGrafo origen;
+    private NodoGrafo destino;
+    private String etiqueta;
+
+    public Arista(NodoGrafo origen, NodoGrafo destino, String etiqueta) {
+        this.origen = origen;
+        this.destino = destino;
+        this.etiqueta = etiqueta;
+    }
+
+    public NodoGrafo getOrigen() {
+        return origen;
+    }
+
+    public NodoGrafo getDestino() {
+        return destino;
+    }
+
+    @Override
+    public int compareTo(Arista otra) {
+        int comparable = this.origen.compareTo(otra.origen);
+        if (comparable != 0) return comparable;
+
+        comparable = this.destino.compareTo(otra.destino);
+        if (comparable != 0) return comparable;
+
+        return this.etiqueta.compareTo(otra.etiqueta);
+    }
+}
---
+++ src/Nodos/NodoGrafo.java
@@ -0,0 +1,35 @@
+package Nodos;
+
+public class NodoGrafo implements Comparable<NodoGrafo> {
+
+    private String valor;
+
+    public NodoGrafo(String valor) {
+        this.valor = valor;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    @Override
+    public int compareTo(NodoGrafo otro) {
+        return this.valor.compareTo(otro.valor);
+    }
+
+    @Override
+    public boolean equals(Object objeto) //recibe un objeto, necesitas comprobar si puedes tratar este objeto como un NodoGrafo
+    {
+        if (objeto instanceof NodoGrafo) //Comprueba el tipo
+        {
+            NodoGrafo otroObjeto = (NodoGrafo) objeto;// Haces el casting, esto convierte el Object en NodoGrafo
+            return this.valor.equals(otroObjeto.valor);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return valor;
+    }
+}
